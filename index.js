373a6287f098d6e519aea12f54126c8471fa79d1
Deepak Yadav <deepak.yadav@linkmobility.com>
2024-04-07 04:22:21 +0530

    basic setup

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send({"health":"OK"}));
+
+app.listen(process.env.SERVER_PORT, () => {
+	console.log(`server running on ${process.env.SERVER_PORT}`);
+});
